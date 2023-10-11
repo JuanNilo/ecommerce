07feb658c51f9a0cdb88fd3437c99792ff025d7c
JuanNilo <juan.nilo@alumnos.ucn.cl>
2023-10-10 21:01:05 -0300

    Update: Products.tsx

--- app/components/home/Products.tsx
+++ app/components/home/Products.tsx
@@ -6,19 +6,20 @@ import Link from "next/link";
 
 const {tertiary, brand} = Colors
 
+const productos = products
 export default async function Products() {  
 
     return (
         <div className="w-[100%] h-[100%] text-center">
-            <h2 className=" text-3xl mb-6" style={{color: tertiary}}>Productos</h2>
-            <ul className="grid grid-cols-4 gap-4">
-              {products.map((product )=> (
+            <h2 className=" text-3xl mb-6" style={{color: tertiary}}>Mas vendidos</h2>
+            <ul className="grid grid-cols-2 gap-4 md:grid-cols-4 xl:grid-cols-4">
+              {productos.map((product )=> (
                 <Link href={`productos/${product.id}`} key={product.id} style={{backgroundColor: brand}} className=" text-center rounded-xl">
                   <div className="h-[20%] px-2">
-                      <p className=" text-2xl py-3 font-bold"
+                      <h2 className=" text-lg md:text-xl 2xl:text-2xl py-3 font-bold"
                         style={{color: tertiary}}>
                           {product.nombre}
-                      </p>
+                      </h2>
                    </div>
                    <div className=" bg-white h-[60%] flex items-center justify-center">
                     <img src={product.imagen} alt="" className="h-[80%]"/>
--- app/components/home/categories/category-nav.tsx
+++ app/components/home/categories/category-nav.tsx
@@ -5,13 +5,15 @@ import Link from "next/link";
 const {primary, secondary, tertiary} = Colors;
 
 export default function CategoryNav(){
+
+    const dataCategorias = categorias
     return(
         <div 
             style={{backgroundColor: primary}}
             className="h-[70%] w-[100%] my-4 rounded-md">
         {/* Contenido */}
         <ul className="flex w-[80%] h-[100%] justify-center gap-6 mx-auto">
-            {categorias.map((categoria )=> (
+            {dataCategorias.map((categoria )=> (
                 <Link href={`/categorias/${categoria.nombre}`} key={categoria.nombre} className="w-[100px] flex items-center justify-center rounded-md" style={{backgroundColor: tertiary}} >
                     <img className="h-[80%] m-auto" src={categoria.imagen} alt={categoria.nombre}/>
                 </Link>
